fed9fbe14fd719c7fdcc70350ebe6481c28eae42
Jihoon Lee <jihoonlee.in@gmail.com>
2017-09-23 21:51:36 +0200

    log level uses string

--- scripts/regression/logistic_regression_mnist.py
+++ scripts/regression/logistic_regression_mnist.py
@@ -5,7 +5,7 @@ from tensorflow.examples.tutorials.mnist import input_data
 from libs import utils
 
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = 2
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 # import matplotlib.pyplot as plt
 
